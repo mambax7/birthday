63bb01994f88894c25105f1846d20e8ee69d23b8
mambax7 <mambax7@gmail.com>
2017-07-22 05:18:07 -0400

    updates

--- admin/about.php
+++ admin/about.php
@@ -9,21 +9,18 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
- * @copyright    The XOOPS Project (http://www.xoops.org)
- * @license   {@link http://www.gnu.org/licenses/gpl-2.0.html GNU Public License}
- * @package    birthday
+ * @copyright    XOOPS Project (https://xoops.org)
+ * @license      {@link http://www.gnu.org/licenses/gpl-2.0.html GNU Public License}
+ * @package      birthday
  * @since        2.5.0
- * @author     XOOPS Module Team
- * @version    $Id $
+ * @author       XOOPS Module Team
  **/
 
-include_once dirname(__FILE__) . '/admin_header.php';
-
+require_once __DIR__ . '/admin_header.php';
 xoops_cp_header();
 
-$aboutAdmin = new ModuleAdmin();
+$adminObject->displayNavigation(basename(__FILE__));
+\Xmf\Module\Admin::setPaypal('xoopsfoundation@gmail.com');
+$adminObject->displayAbout(false);
 
-echo $aboutAdmin->addNavigation('about.php');
-echo $aboutAdmin->renderAbout('xoopsfoundation@gmail.com', false);
-
-include 'admin_footer.php';
+require_once __DIR__ . '/admin_footer.php';
--- admin/admin_footer.php
+++ admin/admin_footer.php
@@ -9,19 +9,21 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
- * @copyright	The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @copyright           XOOPS Project (https://xoops.org)
  * @license             http://www.gnu.org/licenses/gpl-2.0.html GNU Public License
- * @package	birthday
- * @since		2.5.0
- * @author 	XOOPS Module Team
- * @version	$Id $
-**/
+ * @package             birthday
+ * @since               2.5.0
+ * @author              XOOPS Module Team
+ **/
 
+$pathIcon32      = \Xmf\Module\Admin::iconUrl('', 32);
 echo "<div class='adminfooter'>\n"
-    ."  <div style='text-align: center;'>\n"
-    ."    <a href='http://www.xoops.org' rel='external'><img src='{$pathIcon32}/xoopsmicrobutton.gif' alt='XOOPS' title='XOOPS'></a>\n"
-    ."  </div>\n"
-    ."  " . _AM_MODULEADMIN_ADMIN_FOOTER . "\n"
-    ."</div>";
+     . "  <div style='text-align: center;'>\n"
+     . "    <a href='https://xoops.org' rel='external'><img src='{$pathIcon32}/xoopsmicrobutton.gif' alt='XOOPS' title='XOOPS'></a>\n"
+     . "  </div>\n"
+     . '  '
+     . _AM_MODULEADMIN_ADMIN_FOOTER
+     . "\n"
+     . '</div>';
 
 xoops_cp_footer();
